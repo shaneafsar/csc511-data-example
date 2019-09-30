7bc9b34a526e2f387f163d705e3e49571640ada3
Shane Afsar <safsar@linkedin.com>
2019-09-29 22:56:01 -0400

    Added simple detail route (TODO: need more model info)

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('detail', { path: 'detail/:id' });
+});
 
 export default Router;
--- app/serializers/result.js
+++ app/serializers/result.js
@@ -6,22 +6,34 @@ export default DS.JSONAPISerializer.extend({
     return key;
   },
   normalizeResponse(store, primaryModelClass, payload) {
-    payload.data = payload.Search;
-    payload.data.forEach(item => {
-      item.type = primaryModelClass.modelName; //'result'
-      item.attributes = {
-        Title: item.Title,
-        Year: item.Year,
-        Yype: item.Type,
-        Poster: item.Poster
-      };
-      delete item.Title;
-      delete item.Year;
-      delete item.Type;
-      delete item.Poster;
-    });
+    if (payload.Search) {
+      payload.data = payload.Search;
+      payload.data.forEach(item => {
+        item.type = primaryModelClass.modelName; //'result'
+        item.attributes = {
+          Title: item.Title,
+          Year: item.Year,
+          Yype: item.Type,
+          Poster: item.Poster
+        };
+        delete item.Title;
+        delete item.Year;
+        delete item.Type;
+        delete item.Poster;
+      });
 
-    delete payload.Search;
+      delete payload.Search;
+    } else {
+      payload.data = {};
+      payload.data.imdbID = payload.imdbID;
+      payload.data.type = primaryModelClass.modelName;
+      payload.data.attributes = {
+        Title: payload.Title,
+        Year: payload.Year,
+        Yype: payload.Type,
+        Poster: payload.Poster
+      };
+    }
     return this._super(...arguments);
   }
 });
